5f0799b9fdf159199b4d23d46e8bb9c4d180ae4b
McHenry <lukemchenry311@gmail.com>
2021-09-22 01:45:53 -0600

    inquirer prompts

--- index.js
+++ index.js
@@ -1,42 +1,93 @@
 // packages
 const fs = require('fs');
-const inquirer = require("inquirer");
-const Engineer = require('../lib/Engineer');
-const Manager = require('../lib/Manager');
-const Intern = require('../lib/Intern');
 const path = require('path');
+const inquirer = require("inquirer");
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+// push employee info into sayori
+const sayori = []
 
 const render = require("./page-template.js");
 
-const team = []
-// array of questions for user input
-// createmanager
-    // send up 
-// createteam
-    // send up
-// createengineer
-    // sned up
-// createintern
-    // send up
-// buildteam
 function questions() {
     
-        function createManager() {}
+        function createManager() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'Mname',
+                    message: 'what is the managers name?'
+                },
+                {
+                    type: 'input',
+                    name: 'Mid',
+                    message: 'what is the managers id?'
+                },
+                {
+                    type: 'input',
+                    name: 'Memail',
+                    message: 'what is the managers email?'
+                },
+            ]).then(answers => {
+                const manager = new Manager(answers.Mname, answers.Mid, answers.Memail);
+                sayori.push(manager);
+                createTeam();
+            });
+        };
 
         function createTeam() {}
 
-        function createEngineer() {}
+        function createEngineer() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'Ename',
+                    message: 'what is th engineers name?'
+                },
+                {
+                    type: 'input',
+                    name: 'Eid',
+                    message: 'what is the engineers id?'
+                },
+                {
+                    type: 'input',
+                    name: 'Eemail',
+                    message: 'what is the engineers email?'
+                },
+            ]).then(answers => {
+                const engineer = new Engineer(answers.Ename, answers.Eid, answers.Eemail);
+                sayori.push(engineer);
+                createTeam();
+            })
+        }
 
-        function createIntern() {}
+        function createIntern() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'Iname',
+                    message: 'what is the interns name?'
+                },
+                {
+                    type: 'input',
+                    name: 'Iid',
+                    message: 'what is the interns id?'
+                },
+                {
+                    type: 'input',
+                    name: 'Iemail',
+                    message: 'what is the interns email?'
+                },
+            ]).then(answers => {
+                const intern = new Intern(answers.Iname, answers.Iid, answers.Iemail);
+                sayori.push(intern);
+                createTeam();
+            })
+        }
 
     function buildTeam() {}
-}
-
-
-// create function to write vex.html file
-
-
-// create function to initialize app
-
-
-// function call to initialize app
+    createManager();
+};
+questions();
