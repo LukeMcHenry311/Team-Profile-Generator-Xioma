d6d071ea0e78444252a1127bd5802c3ef7c55ae5
McHenry <lukemchenry311@gmail.com>
2021-09-23 19:08:52 -0600

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Team-Profile-Generator-Xioma
+
+## Table of Contents
+* [Screenshot](#screenshot)
+* [Description](#description)
+* [Technologies](#technologies)
+* [Github Link](#github-link)
+* [Deployed-Application](#deployed-application)
+* [Collaborations](#collaborations)
+* [Struggles](#struggles)
+
+## Screenshot
+![Screenshot 2021-09-23 190537](https://user-images.githubusercontent.com/82482629/134603058-23137445-78c8-45c4-8efa-71b153562947.png)
+## Description
+application using inquirer to build team portfolio dynamically
+## Technologies
+Javascript, HTML, CSS
+## Github-Link
+https://github.com/LukeMcHenry311/Team-Profile-Generator-Xioma
+## Collaborations
+StackOverflow, Youtube, Classmates, ASKBCS
+## Struggles
+My biggest struggle for this assignment was definitely the page template javascript file, i had to go in for office hours and it ended up being a misplaced arrow function, other than that a large struggle for me was also just making the route to src/hive.html from index.js
--- src/page-template.js
+++ src/page-template.js
@@ -1,14 +1,13 @@
 const generateTeam = team => {
     let generateManager = manager => {
         return `
-        <div class="card">Day 1
+        <div class="card">
             <div class="card-header">
-            <div class="card-title"><img class="card-img-top" src="" alt=""></div>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
             </div>
             <div class="card-body">
                 <ul class="list-group">
                     <li class="list-group-item">Name: ${manager.getName()}</li>
-                    <li class="list-group-item">Role: ${manager.getRole()}</li>
                     <li class="list-group-item">ID: ${manager.getId()}</li>
                     <li class="list-group-item">Email: ${manager.getEmail()} </li>
                 </ul>
@@ -18,14 +17,13 @@ const generateTeam = team => {
     };
     let generateEngineer = engineer => {
         return `
-        <div class="card">Day 1
+        <div class="card">
             <div class="card-header">
-            <div class="card-title"><img class="card-img-top" src="" alt=""></div>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
             </div>
             <div class="card-body">
                 <ul class="list-group">
                     <li class="list-group-item">Name: ${engineer.getName()} </li>
-                    <li class="list-group-item">Role: ${engineer.getRole()} </li>
                     <li class="list-group-item">ID: ${engineer.getId()} </li>
                     <li class="list-group-item">Email: ${engineer.getEmail()} </li>
                 </ul>
@@ -35,14 +33,13 @@ const generateTeam = team => {
     };
     let generateIntern = intern => {
         return `
-        <div class="card">Day 1
+        <div class="card">
             <div class="card-header">
-            <div class="card-title"><img class="card-img-top" src="" alt=""></div>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
             </div>
             <div class="card-body">
                 <ul class="list-group">
                     <li class="list-group-item">Name: ${intern.getName()}</li>
-                    <li class="list-group-item">Role: ${intern.getRole()}</li>
                     <li class="list-group-item">ID: ${intern.getId()}</li>
                     <li class="list-group-item">Email: ${intern.getEmail()}</li>
                 </ul>
@@ -74,6 +71,7 @@ module.exports = team => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>blue</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
     </head>
     
     <body>
