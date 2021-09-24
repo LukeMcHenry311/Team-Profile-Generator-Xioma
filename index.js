62d374f4946777884d82269d00f4ad235f79cbee
McHenry <lukemchenry311@gmail.com>
2021-09-23 18:49:29 -0600

    correclty sending now, on line 58 and 60 i had to add =>

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // packages
-const fs = require('fs');
-const path = require('path');
-const inquirer = require("inquirer");
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+const path = require('path');
+const fs = require('fs');
+const inquirer = require("inquirer");
 
 // push employee info into sayori
 const sayori = []
 
-const render = require("./page-template.js");
+// path to go from index to src/hive.html
+const sendit = path.resolve(__dirname, "src");
+const shipit = path.join(sendit, "hive.html");
+
+const build = require("./src/page-template.js");
 
 function questions() {
-    
+        createManager();
         function createManager() {
             inquirer.prompt([
                 {
@@ -33,6 +37,7 @@ function questions() {
             ]).then(answers => {
                 const manager = new Manager(answers.Mname, answers.Mid, answers.Memail);
                 sayori.push(manager);
+                console.log(sayori);
                 createTeam();
             });
         };
@@ -41,20 +46,20 @@ function questions() {
             inquirer.prompt([
                 {
                     type: 'list',
-                    name: 'natsuki',
+                    name: 'code',
                     message: 'engineer, intern, or done?',
-                    choices: ['engineer', 'intern', 'done'],
+                    choices: ['Engineer', 'Intern', 'done'],
                 },
             ]).then(response => {
-                switch (response.natsuki) {
-                    case 'engineer':
+                switch (response.code) {
+                    case 'Engineer':
                         createEngineer();
                         break;
-                    case 'intern':
+                    case 'Intern':
                         createIntern();
                         break;
                     default:
-                        createTeam();
+                        buildTeam();
                 }
             })
         }
@@ -79,6 +84,7 @@ function questions() {
             ]).then(answers => {
                 const engineer = new Engineer(answers.Ename, answers.Eid, answers.Eemail);
                 sayori.push(engineer);
+                console.log(sayori);
                 createTeam();
             })
         }
@@ -103,11 +109,15 @@ function questions() {
             ]).then(answers => {
                 const intern = new Intern(answers.Iname, answers.Iid, answers.Iemail);
                 sayori.push(intern);
+                console.log(sayori);
                 createTeam();
             })
         }
 
-    function buildTeam() {}
-    createManager();
+    function buildTeam() {
+        console.log(sayori);
+        fs.writeFileSync(shipit, build(sayori));
+    }
+    
 };
 questions();
---
+++ src/page-template.js
@@ -0,0 +1,96 @@
+const generateTeam = team => {
+    let generateManager = manager => {
+        return `
+        <div class="card">Day 1
+            <div class="card-header">
+            <div class="card-title"><img class="card-img-top" src="" alt=""></div>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">Name: ${manager.getName()}</li>
+                    <li class="list-group-item">Role: ${manager.getRole()}</li>
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: ${manager.getEmail()} </li>
+                </ul>
+            </div>
+            </div>
+        `;
+    };
+    let generateEngineer = engineer => {
+        return `
+        <div class="card">Day 1
+            <div class="card-header">
+            <div class="card-title"><img class="card-img-top" src="" alt=""></div>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">Name: ${engineer.getName()} </li>
+                    <li class="list-group-item">Role: ${engineer.getRole()} </li>
+                    <li class="list-group-item">ID: ${engineer.getId()} </li>
+                    <li class="list-group-item">Email: ${engineer.getEmail()} </li>
+                </ul>
+            </div>
+            </div>
+        `;
+    };
+    let generateIntern = intern => {
+        return `
+        <div class="card">Day 1
+            <div class="card-header">
+            <div class="card-title"><img class="card-img-top" src="" alt=""></div>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">Name: ${intern.getName()}</li>
+                    <li class="list-group-item">Role: ${intern.getRole()}</li>
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: ${intern.getEmail()}</li>
+                </ul>
+            </div>
+            </div>
+        `;
+    };
+
+    const documenti = [];
+
+    documenti.push(team.filter(employee => employee.getRole() === "manager")
+    .map(manager => generateManager(manager)));
+    documenti.push(team.filter(employee =>  employee.getRole() === "intern")
+    .map(intern => generateIntern(intern)));
+    documenti.push(team.filter(employee => employee.getRole() === "engineer")
+    .map(engineer => generateEngineer(engineer)));
+        // .join is smashing together the three employee classes.
+        return documenti.join("");
+};
+
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>blue</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    
+    <body>
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+             </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                    ${generateTeam(team)}
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+    `
+}
